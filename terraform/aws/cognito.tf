fdd5f44224dfbaa8dce370b1317fc18655ae6358
Pat Heard <patrick.heard@cds-snc.ca>
2024-02-21 04:37:11 +0000

    fix: remove Cognito as an IDP

--- terraform/aws/cognito.tf
+++ terraform/aws/cognito.tf
@@ -34,7 +34,7 @@ resource "aws_cognito_identity_provider" "google" {
 resource "aws_cognito_user_pool_client" "cognito" {
   name                                 = "client"
   user_pool_id                         = aws_cognito_user_pool.users.id
-  supported_identity_providers         = ["COGNITO", aws_cognito_identity_provider.google.provider_name]
+  supported_identity_providers         = [aws_cognito_identity_provider.google.provider_name]
   allowed_oauth_flows_user_pool_client = true
   allowed_oauth_flows                  = ["code"]
   allowed_oauth_scopes                 = ["email", "openid", "profile"]
